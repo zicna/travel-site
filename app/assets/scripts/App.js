ed050f9662784d49dacb1c2283cc1fcbeef6f77b
Zicko <milanzivkovic1987@gmail.com>
2020-10-26 15:14:29 -0500

    mobile menu all done

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,37 +1,11 @@
 import '../styles/styles.css';
-import Person from './modules/Person';
+import MobileMenu from './modules/MobileMenu' 
+
+let mobileMenu = new MobileMenu();
+
 
 if (module.hot) {
     module.hot.accept()
 }
 
-/*lesson example code below this line */
 
-
-
-// let john = {
-//  name: "John Doe",
-//  favoriteColor: "blue",
-//  greet: function() {
-//      console.log("Hello my name is " + this.name + " and my favorite color is " + this.favoriteColor + ".");
-//  }
-// }
-
-// john.greet();
-class Adult extends Person {
-    payTaxes() {
-        console.log(this.name + " now owns zero taxes.");
-    }
-
-}
-
-let john = new Person("John Doe", "purple");
-john.greet();
-
-let jane = new Adult("Jane Doe", "red");
-jane.greet();
-jane.payTaxes();
-
-
-// person(John.name, John.favoriteColor);
-// person ("Jane Smith", "green");
