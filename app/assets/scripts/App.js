b7441daeebed8c7f8cd74934176da7e3f54d13f4
Zicko <milanzivkovic1987@gmail.com>
2020-10-27 00:17:34 -0500

    completed the reveal on scroll behavior

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,12 @@
 import '../styles/styles.css';
 import MobileMenu from './modules/MobileMenu' 
+import RevealOnScroll from './modules/RevealOnScroll'
+
+
+
+new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
+new RevealOnScroll(document.querySelectorAll(".features-item"), 75)
+
 
 let mobileMenu = new MobileMenu();
 
